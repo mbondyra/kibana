b33be95a80f2655c2774ac513985777fc5035799
Marta Bondyra <marta.bondyra@elastic.co>
2022-03-08 13:09:50 +0100

    changing API for config_panel

--- x-pack/plugins/lens/public/xy_visualization/visualization.test.ts
+++ x-pack/plugins/lens/public/xy_visualization/visualization.test.ts
@@ -9,7 +9,12 @@ import { getXyVisualization } from './visualization';
 import { Position } from '@elastic/charts';
 import { Operation, VisualizeEditorContext, Suggestion } from '../types';
 import type { State, XYSuggestion } from './types';
-import type { SeriesType, XYDataLayerConfig, XYLayerConfig } from '../../common/expressions';
+import type {
+  SeriesType,
+  XYDataLayerConfig,
+  XYLayerConfig,
+  XYReferenceLineLayerConfig,
+} from '../../common/expressions';
 import { layerTypes } from '../../common';
 import { createMockDatasource, createMockFramePublicAPI } from '../mocks';
 import { LensIconChartBar } from '../assets/chart_bar';
@@ -422,7 +427,7 @@ describe('xy_visualization', () => {
         context,
       });
       expect(state?.layers[0]).toHaveProperty('seriesType', 'area');
-      expect(state?.layers[0].yConfig).toStrictEqual([
+      expect((state?.layers[0] as XYDataLayerConfig).yConfig).toStrictEqual([
         {
           axisMode: 'right',
           color: '#68BC00',
@@ -1022,7 +1027,7 @@ describe('xy_visualization', () => {
       it('should support static value', () => {
         const state = getStateWithBaseReferenceLine();
         state.layers[0].accessors = [];
-        state.layers[1].yConfig = undefined;
+        (state.layers[1] as XYReferenceLineLayerConfig).yConfig = undefined;
         expect(
           xyVisualization.getConfiguration({
             state: getStateWithBaseReferenceLine(),
@@ -1035,7 +1040,7 @@ describe('xy_visualization', () => {
       it('should return no referenceLine groups for a empty data layer', () => {
         const state = getStateWithBaseReferenceLine();
         state.layers[0].accessors = [];
-        state.layers[1].yConfig = undefined;
+        (state.layers[1] as XYReferenceLineLayerConfig).yConfig = undefined;
 
         const options = xyVisualization.getConfiguration({
           state,
@@ -1059,8 +1064,8 @@ describe('xy_visualization', () => {
 
       it('should return a group for the vertical right axis', () => {
         const state = getStateWithBaseReferenceLine();
-        state.layers[0].yConfig = [{ axisMode: 'right', forAccessor: 'a' }];
-        state.layers[1].yConfig![0].axisMode = 'right';
+        (state.layers[0] as XYDataLayerConfig).yConfig = [{ axisMode: 'right', forAccessor: 'a' }];
+        (state.layers[1] as XYReferenceLineLayerConfig).yConfig![0].axisMode = 'right';
 
         const options = xyVisualization.getConfiguration({
           state,
@@ -1076,7 +1081,7 @@ describe('xy_visualization', () => {
         const state = getStateWithBaseReferenceLine();
         (state.layers[0] as XYDataLayerConfig).xAccessor = 'b';
         (state.layers[0] as XYDataLayerConfig).accessors = [];
-        state.layers[1].yConfig = []; // empty the configuration
+        (state.layers[1] as XYReferenceLineLayerConfig).yConfig = []; // empty the configuration
         // set the xAccessor as date_histogram
         frame.datasourceLayers.referenceLine.getOperationForColumnId = jest.fn((accessor) => {
           if (accessor === 'b') {
@@ -1103,7 +1108,7 @@ describe('xy_visualization', () => {
         const state = getStateWithBaseReferenceLine();
         (state.layers[0] as XYDataLayerConfig).xAccessor = 'b';
         (state.layers[0] as XYDataLayerConfig).accessors = [];
-        state.layers[1].yConfig![0].axisMode = 'bottom';
+        (state.layers[1] as XYReferenceLineLayerConfig).yConfig![0].axisMode = 'bottom';
         // set the xAccessor as date_histogram
         frame.datasourceLayers.referenceLine.getOperationForColumnId = jest.fn((accessor) => {
           if (accessor === 'b') {
@@ -1140,7 +1145,7 @@ describe('xy_visualization', () => {
           { axisMode: 'right', forAccessor: 'b' },
           { axisMode: 'left', forAccessor: 'a' },
         ];
-        state.layers[1].yConfig = [
+        (state.layers[1] as XYReferenceLineLayerConfig).yConfig = [
           { forAccessor: 'c', axisMode: 'bottom' },
           { forAccessor: 'b', axisMode: 'right' },
           { forAccessor: 'a', axisMode: 'left' },
@@ -1173,7 +1178,7 @@ describe('xy_visualization', () => {
         const state = getStateWithBaseReferenceLine();
         (state.layers[0] as XYDataLayerConfig).xAccessor = 'b';
         (state.layers[0] as XYDataLayerConfig).accessors = [];
-        state.layers[1].yConfig = []; // empty the configuration
+        (state.layers[1] as XYReferenceLineLayerConfig).yConfig = []; // empty the configuration
         // set the xAccessor as top values
         frame.datasourceLayers.referenceLine.getOperationForColumnId = jest.fn((accessor) => {
           if (accessor === 'b') {
@@ -1200,7 +1205,7 @@ describe('xy_visualization', () => {
         const state = getStateWithBaseReferenceLine();
         (state.layers[0] as XYDataLayerConfig).xAccessor = 'b';
         (state.layers[0] as XYDataLayerConfig).accessors = [];
-        state.layers[1].yConfig![0].axisMode = 'bottom';
+        (state.layers[1] as XYReferenceLineLayerConfig).yConfig![0].axisMode = 'bottom';
         // set the xAccessor as date_histogram
         frame.datasourceLayers.referenceLine.getOperationForColumnId = jest.fn((accessor) => {
           if (accessor === 'b') {
@@ -1264,7 +1269,7 @@ describe('xy_visualization', () => {
 
         const state = getStateWithBaseReferenceLine();
         (state.layers[0] as XYDataLayerConfig).accessors = ['yAccessorId', 'yAccessorId2'];
-        state.layers[1].yConfig = []; // empty the configuration
+        (state.layers[1] as XYReferenceLineLayerConfig).yConfig = []; // empty the configuration
 
         const options = xyVisualization.getConfiguration({
           state,
