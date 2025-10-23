fa7e46e09c4eaf16aa8af8ef47d3e6f7bdf10dea
mbondyra <marta.bondyra@elastic.co>
2025-10-23 17:09:23 +0200

    fix types

--- src/platform/plugins/shared/dashboard/server/content_management/v1/transforms/out/transform_references_out.ts
+++ src/platform/plugins/shared/dashboard/server/content_management/v1/transforms/out/transform_references_out.ts
@@ -8,14 +8,14 @@
  */
 
 import type { SavedObjectReference } from '@kbn/core/server';
-import type { DashboardAttributes, DashboardPanel } from '../../types';
+import type { DashboardPanel } from '../../types';
 import { isDashboardSection } from '../../../../../common';
 import { embeddableService } from '../../../../kibana_services';
 import { getPanelIdFromReference } from '../../../../../common/reference_utils';
 
 export function transformReferencesOut(
   references: SavedObjectReference[],
-  panels?: DashboardAttributes['panels']
+  panels?: DashboardPanel[]
 ): SavedObjectReference[] {
   // key: panel uid
   // value: boolean indicating to drop references for panel
